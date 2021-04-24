cd5a1eeffeca9bfac0cf2807d7ad5593255ee0ea
Rubens <rubao.ma@gmail.com>
2021-04-23 21:43:08 -0300

    create menu options

--- app/src/main/java/com/rubdev/recycleviewcard/MainActivity.kt
+++ app/src/main/java/com/rubdev/recycleviewcard/MainActivity.kt
@@ -2,6 +2,10 @@ package com.rubdev.recycleviewcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -31,15 +35,46 @@ class MainActivity : AppCompatActivity() {
             arrayListOf(
                 Contact(
                     "Rubens Moura",
-                    "(85)98203-8002",
+                    "(85)99999-9992",
                     "img.jpg"
                 ),
                 Contact(
                     "Talita Moura",
-                    "(85)98119-8001",
+                    "(85)90999-0001 ",
                     "img.jpg"
                 )
             )
         )
+    }
+
+    // Metodo que infla o layout do menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(
+            this,
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    // metodo que observa os itens dos menus observando os toques nos itens do menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
     }
 }
